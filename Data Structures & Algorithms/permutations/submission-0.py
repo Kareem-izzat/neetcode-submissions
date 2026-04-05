36c04dca9e8f8102e674ed5cfc1b9e6b9a503490
Kareem Al-Qutob <97916953+Kareem-izzat@users.noreply.github.com>
2026-04-05 18:59:08 +0300

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        ans=[]
+        index=0
+        def backtrack(index,arr):
+            if len(arr)==len(nums):
+                ans.append(arr)
+                return
+
+            for i in range(0,len(nums)):
+                if i != index and nums[i] not in arr:
+                    cpy=arr.copy()
+                    cpy.append(nums[i])
+                    backtrack(i, cpy)
+        for i in range(0,len(nums)):
+            backtrack(i,[nums[i]])
+        return ans
+        
